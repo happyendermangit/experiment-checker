c5104eaa155e12bd53cfe9ecc43934b5c53fdb1b
Happy enderman <66224387+happyendermangit@users.noreply.github.com>
2023-05-09 21:15:28 +0200

    Create experiments.js

---
+++ experiments.js
@@ -0,0 +1,7 @@
+/ * CREIDTS TO HAPPY ENDERMAN (AKA | __.JS.__ |) */
+/ * CREIDTS TO HAPPY ENDERMAN (AKA | __.JS.__ |) */
+/ * CREIDTS TO HAPPY ENDERMAN (AKA | __.JS.__ |) */
+const experiments = [] 
+/ * CREIDTS TO HAPPY ENDERMAN (AKA | __.JS.__ |) */
+/ * CREIDTS TO HAPPY ENDERMAN (AKA | __.JS.__ |) */
+/ * CREIDTS TO HAPPY ENDERMAN (AKA | __.JS.__ |) */
