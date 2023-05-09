a9e3067db267940cbeecd4d29bfd9449c39ad261
Happy enderman <66224387+happyendermangit@users.noreply.github.com>
2023-05-09 23:09:58 +0200

    Update experiments.js

--- experiments.js
+++ experiments.js
@@ -1,7 +1,3 @@
 / * CREIDTS TO HAPPY ENDERMAN (AKA | __.JS.__ |) */
-/ * CREIDTS TO HAPPY ENDERMAN (AKA | __.JS.__ |) */
-/ * CREIDTS TO HAPPY ENDERMAN (AKA | __.JS.__ |) */
+
 const experiments = [] 
-/ * CREIDTS TO HAPPY ENDERMAN (AKA | __.JS.__ |) */
-/ * CREIDTS TO HAPPY ENDERMAN (AKA | __.JS.__ |) */
-/ * CREIDTS TO HAPPY ENDERMAN (AKA | __.JS.__ |) */
